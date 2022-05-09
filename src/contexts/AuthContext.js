4590207496aa3ea84fdfc4ad38f8c67d573ffe72
prakhargvp <prakhargvp@gmail.com>
2022-05-09 10:15:04 +0530

    feat: added basic signup page

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  function signup(email, password) {
+    auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
